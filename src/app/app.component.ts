7b89a2817008b75f34ef652e945dd41eccc18c91
Ankit Jindal <ankit28jindal@gmail.com>
2018-10-14 16:52:55 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sentiment-analysis';
+  private title: String = 'sentiment-analysis';
+  searchText: String;
+  items: Array<object>;
+
+  constructor(private http: HttpClient) {
+    this.items = [
+      {
+        'id': 'title1',
+        'title': 'Title 1',
+        'description': 'Description 1'
+      },
+      {
+        'id': 'title2',
+        'title': 'Title 2',
+        'description': 'Description 2'
+      },
+      {
+        'id': 'title3',
+        'title': 'Title 3',
+        'description': 'Description 3'
+      }
+    ];
+  }
+
+  getData() {
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Methods': 'GET',
+      'Content-Type':  'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+
+    this.http.get(`http://127.0.0.1:5000/user/${this.searchText}/10`, {headers})
+    .subscribe((res) => {
+      console.log('Got the Response');
+      console.log(res);
+    });
+  }
 }
